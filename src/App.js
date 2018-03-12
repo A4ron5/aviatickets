db6490e148c2154d817ae80d8ff9cc4d3a0ea66a
Max <a4ron5@yandex.ru>
2018-03-12 15:27:54 +0500

    add axios, fix ui, add tickets feat

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React, { Component } from "react";
-import { Ticket } from "./ui/organisms/ticket";
-import "./App.css";
+import { Tickets } from "./features/ticket-list/tickets";
 
 class App extends Component {
   render() {
-    return (
-      <Ticket
-        price={"23200"}
-        timeFrom={"09:25"}
-        cityFrom={"VVO, Владивосток"}
-        dateFrom={"9 окт 2016, Пт"}
-        transfer={"2"}
-        timeTo={"09:25"}
-        cityTo={"VVO, Владивосток"}
-        dateTo={"9 окт 2016, Пт"}
-      />
-    );
+    return <Tickets />;
   }
 }
 
---
+++ src/features/ticket-list/tickets.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Ticket } from "../../ui/organisms";
+import data from "./tickets.json";
+
+export class Tickets extends React.Component {
+  state = {
+    ticketsItem: []
+  };
+
+  componentDidMount() {}
+
+  render() {
+    const tickets = data.tickets.map(item => {
+      return (
+        <Ticket
+          price={item.price}
+          timeFrom={item.departure_time}
+          cityFrom={`${item.origin},${item.origin_name}`}
+          dateFrom={item.departure_date}
+          transfer={item.stops}
+          timeTo={item.arrival_time}
+          cityTo={`${item.destination},${item.destination_name}`}
+          dateTo={item.arrival_date}
+        />
+      );
+    });
+    return <React.Fragment>{tickets}</React.Fragment>;
+  }
+}
