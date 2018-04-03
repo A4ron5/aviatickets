4e2ed323612660033bdcd64a37036a990505b252
Max <a4ron5@yandex.ru>
2018-04-03 17:49:51 +0500

    logic: add actions

---
+++ src/actions.js
@@ -0,0 +1,19 @@
+export const all = () => ({
+  type: 'SELECT_ALL'
+})
+
+export const nullStops = () => ({
+  type: 'SELECT_NULL'
+})
+
+export const oneStop = () => ({
+  type: 'SELECT_ONE'
+})
+
+export const twoStops = () => ({
+  type: 'SELECT_TWO'
+})
+
+export const threeStops = () => ({
+  type: 'SELECT_THREE'
+})
